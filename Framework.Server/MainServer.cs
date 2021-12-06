5548b16954d20fc4f0cad3f0c9f18e42592cb342
Sampleeeee <behnersam@gmail.com>
2021-12-06 18:12:23 -0500

    Character Dictionary

--- Framework.Server/MainServer.cs
+++ Framework.Server/MainServer.cs
@@ -1,19 +1,19 @@
-﻿using CitizenFX.Core;
+﻿using System.Collections.Generic;
+using CitizenFX.Core;
 using Framework.Shared;
 using Newtonsoft.Json;
+using System;
+// using Framework.Shared.JsonConverters;
 
 namespace Framework.Server;
 
-// ReSharper disable once UnusedType.Global
+// ReSharper disable once ClassNeverInstantiated.Global
 public class MainServer : BaseScript
 {
+    public static Dictionary<Player, Character> Characters = new();
+    
     public MainServer()
     {
         Log.Info( "Server has started" );
-
-        Log.Info( JsonConvert.SerializeObject( new
-        {
-            Test = "Test"
-        } ) );
     }
 }
