f7701e5d59abfaee2afe64d12d159898c2477c40
Sampleeeee <behnersam@gmail.com>
2021-12-06 18:12:17 -0500

    Send player character on join

---
+++ Framework.Client/Handlers/CharacterHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using CitizenFX.Core;
+using Framework.Shared;
+using Framework.Shared.JsonConverters;
+using Newtonsoft.Json;
+
+namespace Framework.Client.Handlers;
+
+public class CharacterHandler : BaseScript
+{
+    [EventHandler( "SetupCharacter" )]
+    private void SetupCharacter( string json )
+    {
+        Log.Info( "Got SetupCharacter Event" );
+        
+        Log.Info( json );
+
+        var character = JsonConvert.DeserializeObject<Character>( json, new CharacterJsonConverter() );
+
+        Log.Info( character );
+        MainClient.Character = character;
+        
+        Log.Info( "Assigned Character" );
+    }
+}
---
+++ Framework.Server/Handlers/CharacterHandler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using CitizenFX.Core;
+using Framework.Shared;
+using Newtonsoft.Json;
+
+namespace Framework.Server.Handlers;
+
+// ReSharper disable once UnusedType.Global
+public class CharacterHandler : BaseScript
+{
+    [EventHandler( "PlayerReady" )]
+    private void OnPlayerReady( [FromSource] Player player )
+    {
+        Log.Info( "Got server event PlayerReady" );
+
+        var character = new Character
+        {
+            FirstName = "Jon",
+            LastName = "Doe",
+            Cash = 10_000,
+            Bank = 10_000,
+            DirtyMoney = 10_000,
+            Hunger = 100,
+            Thirst = 100,
+            DateOfBirth = DateTime.Parse( "02/18/2000" ),
+            Gender = Gender.Male,
+        };
+
+        string json = JsonConvert.SerializeObject( character );
+
+        Log.Info( json );
+        
+        player.TriggerEvent( "SetupCharacter", json );
+
+        character.Player = player;
+        MainServer.Characters[player] = character;
+
+        Log.Info( "Sent Back Character" );
+    }
+}
