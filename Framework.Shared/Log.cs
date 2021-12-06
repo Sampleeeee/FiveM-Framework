543feeee219ebf0378df9429d6b0694609f5235e
Sampleeeee <behnersam@gmail.com>
2021-12-06 18:11:13 -0500

    Added ability for line numbers when logging

--- Framework.Shared/Log.cs
+++ Framework.Shared/Log.cs
@@ -1,28 +1,53 @@
+using System;
+using System.Runtime.CompilerServices;
 using CitizenFX.Core;
 
 namespace Framework.Shared;
 
 public static class Log
 {
-    public static void Info( object obj )
+    public static void Info( object obj, bool showPath = false, [CallerFilePath] string path = "Unknown File",
+        [CallerLineNumber] int line = -1 )
     {
-        if ( obj is "" ) return;
-        
-        Debug.WriteLine( "^4[INFO]^0: " + obj );
-    }
-        
 
-    public static void Warning( object obj )
-    {
         if ( obj is "" ) return;
-        
-        Debug.WriteLine( "^3[WARNING]^0: " + obj );
+        path = ShortenPath( path );
+
+        Debug.WriteLine( $"^5[INFO]^0{( showPath ? $"[{path}:{line}" : "" )}: {obj}" );
     }
 
-    public static void Error( object obj )
+
+    public static void Warning( object obj, bool showPath = false, [CallerFilePath] string path = "Unknown File",
+        [CallerLineNumber] int line = -1 )
     {
+
         if ( obj is "" ) return;
-        
-        Debug.WriteLine( "^1[ERROR]^0: " + obj );
+        path = ShortenPath( path );
+
+        Debug.WriteLine( $"^4[WARNING]^0{( showPath ? $"[{path}:{line}]" : "" )}: {obj}" );
+    }
+
+    public static void Error( object obj, [CallerFilePath] string path = "Unknown File",
+        [CallerLineNumber] int line = -1 )
+    {
+
+        if ( obj is "" ) return;
+        path = ShortenPath( path );
+
+        Debug.WriteLine( $"^1[ERROR]^0[{path}:{line}]: {obj}" );
+    }
+
+    private static string ShortenPath( string path )
+    {
+        path = path.Replace( "\\", "/" );
+
+        try
+        {
+            return path.Substring( path.IndexOf( "/Framework", StringComparison.Ordinal ) + "Framework".Length + 2 );
+        }
+        catch
+        {
+            return path;
+        }
     }
 }
